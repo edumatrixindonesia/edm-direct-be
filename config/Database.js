a12b37e107983ee2d5513b20f47310eaab53ee31
edumatrix_dev <eduwebdev@gmail.com>
2024-03-19 16:40:47 +0700

    add vercel file

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  "u1665540_edm_database_directory",
-  "u1665540_admin",
-  "Bismillah2023",
-  {
-    host: "server.edumatrix-indonesia.com",
-    dialect: "mysql",
-  }
-);
+// const db = new Sequelize("u1665540_edm_database_directory", 'u1665540_admin', 'Bismillah2023', {
+//   host: "server.edumatrix-indonesia.com",
+//   dialect: "mysql",
+// });
+
+const db = new Sequelize("edm_database", 'root', '', {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 export default db;
--- routes/IbuKotaKabRoute.js
+++ routes/IbuKotaKabRoute.js
@@ -16,28 +16,3 @@ router.patch("/ibukotakab/:id", updateIbuKota);
 router.delete("/ibukotakab/:id", deleteIbuKota);
 
 export default router;
-
-// const express = require("express");
-// const router = express.Router();
-// const { Kota, District } = require("../models");
-
-// Create a new city
-// router.post("/", async (req, res) => {
-//   try {
-//     const newKota = await Kota.create({
-//       name: req.body.name,
-//       slug: req.body.slug,
-//       image: req.body.image,
-//       url: req.body.url,
-//     });
-//     res.status(200).json(newKota);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-// Create a new district within a city
-// router.post("/:id/districts", async (req, res) => {
-//   try {
-//     const newDistrict = await District.create({
-//       name
